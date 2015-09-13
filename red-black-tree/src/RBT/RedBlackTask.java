08ebdb546296ba9e9d65b17006ff7f28a753063c
spirifoxy <spirit.ixi@gmail.com>
2015-09-14 01:37:24 +0600

    +insert +console output *structure changing

--- red-black-tree/src/RBT/RedBlackTask.java
+++ red-black-tree/src/RBT/RedBlackTask.java
@@ -1,7 +1,14 @@
 package RBT;
 
+import RBT.RedBlackTree.Node;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
 import java.io.*;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
 
 /**
  *
@@ -9,42 +16,101 @@ import java.util.Scanner;
  */
 public class RedBlackTask {
 
-    public static RedBlackTree readFile(String fileName) throws FileNotFoundException {
-        
+    public static Node createNodeFromString(String line) {
+        Node node;
+        String name;
+        int id;
+        int[] ex = new int[5];
+
+        try {
+            String[] str = line.split(" ");
+
+            name = str[0];
+            id = Integer.parseInt(str[1]);
+            for (int i = 0; i < 5; i++) {
+                ex[i] = Integer.parseInt(str[i + 2]);
+            }
+            node = new Node(id, name, ex);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Ошибка ввода");
+            throw new RuntimeException(e);
+        }
+        return node;
+    }
+
+    public static RedBlackTree readFile(String fileName) {
         RedBlackTree rbt = new RedBlackTree();
-        
+        Node node;
         File file = new File(fileName);
         try {
             BufferedReader in = new BufferedReader(new FileReader(file));
             try {
                 String line;
                 while ((line = in.readLine()) != null) {
-                    String[] str = line.split("");
-                    /*
-                    name = str[0];
-                    id = Integer.parseInt(str[1]);
-                    for (int i=2; i<7; i++) {
-                        ex[i] = Integer.parseInt(str[i]);
-                    }
-                    */
+                    node = createNodeFromString(line);
+                    rbt.insert(node);
                 }
-            }
-            finally {
+            } finally {
                 in.close();
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return rbt;
     }
-    
-    
-    
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        RedBlackTree rbt = new RedBlackTree();
+
+        rbt = readFile("tree.dat");
+
+        Scanner scan = new Scanner(System.in);
         
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        while (true) {
+            
+            System.out.println("\n1.- Добавить узел\n"
+                    + "2.- Нарисовать\n"
+                    + "3.- Новое дерево\n"
+                    + "4.- Вывод в консоль\n"
+                    + "5.- Удалить дерево\n");
+            int choice = scan.nextInt();
+
+            String line;
+            Node node;
+            switch (choice) {
+                case 1:
+                    while ( !"stop".equals(line= in.readLine())) {
+                        node = createNodeFromString(line);
+                        rbt.insert(node);
+                    }
+                    rbt.printTree(rbt.getRoot());
+                    break;
+                case 2:
+                    RedBlackViewer frame = new RedBlackViewer(rbt);
+                    frame.setSize(800, 600);
+                    frame.setVisible(true);
+                    break;
+                case 3:
+                    /*
+                    
+                    */
+                    break;
+                case 4:
+                    rbt.printTree(rbt.getRoot());
+                    break;
+                case 5:
+                    /*
+                    
+                    */
+                    break;
+            }
+        }
     }
 }
--- red-black-tree/src/RBT/RedBlackTree.java
+++ red-black-tree/src/RBT/RedBlackTree.java
@@ -5,5 +5,179 @@ package RBT;
  * @author spirifoxy
  */
 public class RedBlackTree {
+
+    private static final int BLACK = 1;
+    private static final int RED = 0;
+
+    public static class Node {
+
+        Node left = nil, right = nil, parent = nil;
+        int color = BLACK;
+        String name;
+        int id;
+        int[] ex = new int[5];
+
+        public Node(int id, String name, int[] ex) {
+            this.id = id;
+            this.name = name;
+            this.ex = ex;
+        }
+        
+        public Node getParent() {
+            return parent;
+        }
+    }
+
+    private static final Node nil = new Node(-1, null, null);
+    private Node root;
+
+    public RedBlackTree() {
+        root = nil;
+    }
+
+    private boolean isEmpty() {
+        return root == nil;
+    }
     
+    public Node getRoot() {
+        return root;
+    }
+    
+    
+    private void deleteTree() {
+        root = nil;
+    }
+
+    public void insert(Node node) {
+        Node temp = root;
+        if (root == nil) {
+            root = node;
+        } else {
+            node.color = RED;
+            while (true) {
+                if (node.id < temp.id) {
+                    if (temp.left == nil) {
+                        temp.left = node;
+                        node.parent = temp;
+                        break;
+                    } else {
+                        temp = temp.left;
+                    }
+                } else if (node.id > temp.id) {
+                    if (temp.right == nil) {
+                        temp.right = node;
+                        node.parent = temp;
+                        break;
+                    } else {
+                        temp = temp.right;
+                    }
+                }
+            }
+            fixTree(node);
+        }
+    }
+
+    private void fixTree(Node node) {
+        while (node.parent.color == RED) {
+            Node uncle = nil;
+            if (node.parent == node.parent.parent.left) {
+                uncle = node.parent.parent.right;
+
+                if (uncle != nil && uncle.color == RED) {
+                    node.parent.color = BLACK;
+                    uncle.color = BLACK;
+                    node.parent.parent.color = RED;
+                    node = node.parent.parent;
+                    continue;
+                }
+                if (node == node.parent.right) {
+                    //нужен двойной поворот
+                    node = node.parent;
+                    rotateLeft(node);
+                }
+                node.parent.color = BLACK;
+                node.parent.parent.color = RED;
+                rotateRight(node.parent.parent);
+            } else {
+                uncle = node.parent.parent.left;
+                if (uncle != nil && uncle.color == RED) {
+                    node.parent.color = BLACK;
+                    uncle.color = BLACK;
+                    node.parent.parent.color = RED;
+                    node = node.parent.parent;
+                    continue;
+                }
+                if (node == node.parent.left) {
+                    //нужен двойной поворот
+                    node = node.parent;
+                    rotateRight(node);
+                }
+                node.parent.color = BLACK;
+                node.parent.parent.color = RED;
+                rotateLeft(node.parent.parent);
+            }
+        }
+        root.color = BLACK;
+    }
+
+    void rotateLeft(Node node) {
+        if (node.parent != nil) {
+            if (node == node.parent.left) {
+                node.parent.left = node.right;
+            } else {
+                node.parent.right = node.right;
+            }
+            node.right.parent = node.parent;
+            node.parent = node.right;
+            if (node.right.left != nil) {
+                node.right.left.parent = node;
+            }
+            node.right = node.right.left;
+            node.parent.left = node;
+        } else {    //поворот корня
+            Node right = root.right;
+            root.right = right.left;
+            right.left.parent = root;
+            root.parent = right;
+            right.left = root;
+            right.parent = nil;
+            root = right;
+        }
+    }
+
+    void rotateRight(Node node) {
+        if (node.parent != nil) {
+            if (node == node.parent.left) {
+                node.parent.left = node.left;
+            } else {
+                node.parent.right = node.left;
+            }
+
+            node.left.parent = node.parent;
+            node.parent = node.left;
+            if (node.left.right != nil) {
+                node.left.right.parent = node;
+            }
+            node.left = node.left.right;
+            node.parent.right = node;
+        } else {    //поворот корня
+            Node left = root.left;
+            root.left = root.left.right;
+            left.right.parent = root;
+            root.parent = left;
+            left.right = root;
+            left.parent = nil;
+            root = left;
+        }
+    }
+
+    
+    public void printTree(Node node) {
+        if (node == nil) {
+            return;
+        }
+        printTree(node.left);
+        System.out.print(((node.color == RED) ? "Цвет: К " : "Цвет: Ч ") + "ID: " + node.id + " Родитель: " + node.parent.id + "\n");
+        printTree(node.right);
+    }
 }
