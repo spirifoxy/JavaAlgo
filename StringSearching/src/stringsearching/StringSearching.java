54d3f6f28756510fd89ec9e47db90a5fbe505f0f
spirifoxy <spirit.ixi@gmail.com>
2015-09-26 15:40:48 +0700

    Initialization

---
+++ StringSearching/src/stringsearching/StringSearching.java
@@ -0,0 +1,16 @@
+package stringsearching;
+
+/**
+ *
+ * @author spirifoxy
+ */
+public class StringSearching {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ SuffixAutomaton/src/suffixautomaton/SuffixAutomaton.java
@@ -0,0 +1,16 @@
+package suffixautomaton;
+
+/**
+ *
+ * @author spirifoxy
+ */
+public class SuffixAutomaton {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
