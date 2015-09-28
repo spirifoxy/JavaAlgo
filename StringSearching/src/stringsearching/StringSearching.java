a0b19c3ca33798a69c219cd6636a8b1b2e555396
spirifoxy <spirit.ixi@gmail.com>
2015-09-28 23:46:42 +0600

    Final changes

--- StringSearching/src/stringsearching/StringSearching.java
+++ StringSearching/src/stringsearching/StringSearching.java
@@ -1,16 +1,57 @@
 package stringsearching;
 
+import java.io.*;
+
 /**
+ * <p>
+ * 13. Сравнить по быстродействию алгоритм Бойера-Мура и алгоритм Рабина-Карпа
+ * при поиске в тексте одного из k образцов</p>
  *
  * @author spirifoxy
  */
 public class StringSearching {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static long getCurrentTime() { //текущее время в мс
+        return System.currentTimeMillis();
     }
-    
+
+    private static long countPastTime(long time) { //прошедшее время в мс
+        return getCurrentTime() - time;
+    }
+
+    public static void main(String[] args) throws IOException {
+        long timeOut;
+        BoyerMoore bm;
+        RabinKarp rk;
+        File textFile;
+        BufferedReader in;
+        String text = "";
+        String pattern, line;
+        textFile = new File("text.txt");
+
+        in = new BufferedReader(new FileReader(textFile));
+        while ((line = in.readLine()) != null) {
+            text += line;
+        }
+
+        in = new BufferedReader(new InputStreamReader(System.in));
+        pattern = in.readLine();
+
+        timeOut = getCurrentTime();
+        bm = new BoyerMoore(pattern);
+        String result = bm.search(text);
+        System.out.println(result);
+        timeOut = countPastTime(timeOut);
+        System.out.println("BM time: " + timeOut);
+
+        timeOut = getCurrentTime();
+        rk = new RabinKarp(pattern);
+        result = rk.search(text);
+        System.out.println(result);
+        timeOut = countPastTime(timeOut);
+        System.out.println("RK time: " + timeOut);
+
+        in.close();
+    }
+
 }
